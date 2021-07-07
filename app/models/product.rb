0223723f850c4253d91ae901abf2a4a582886dbb
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-07 22:20:36 +0900

    ProductモデルにCartProductモデルとのアソシエーション設定
    ProductモデルとCartProductモデルは1対多の関係なので、Productモデルにhas_manyを記述。
    dependent: :destroyは親モデルを削除した際に、その親モデルに紐づく子モデルも削除できるようにするために記述。

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   mount_uploader :image, ImageUploader
+
+  has_many :cart_products, dependent: :destroy
 end
