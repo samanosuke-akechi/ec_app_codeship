29247f76e7e8c24d49c47d2444d1dc3e95e123db
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-08 14:32:11 +0900

    買い物カゴに入れた後のリダイレクト先をカート内商品一覧ページに変更

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -8,6 +8,6 @@ class CartProductsController < ApplicationController
     cart = current_user.cart || current_user.create_cart  # ログインユーザーがカートを持っていればそれを変数に代入、持っていなければオブジェクトを生成して代入
     product = Product.find_by(id: params[:product_id])  # パラメーターで送られてきた商品IDを元にレコードを代入
     cart.cart_products.create(product_id: product.id)  # create_cartは、has_one関連づけをすると使用できるメソッドで、.create_<関連先>とすることでオブジェクトを生成できる。つまりこの場合はログインユーザーに紐づくカートが生成される（cartsテーブルにuser_idが保存される）。
-    redirect_to product_path(product), notice: "カートに追加しました"
+    redirect_to cart_products_path, notice: "カートに追加しました"
   end
 end
