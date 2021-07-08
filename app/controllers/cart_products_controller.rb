ca38938aec0ecd7f212d0a47f1ad4bdb1442b0c9
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-08 13:07:48 +0900

    「買い物カゴに入れる」ボタンを押した時の処理
    条件分岐にてログインユーザーが買い物カゴを持っているか判定。
    持っていない場合は、
    current_user.create_cart
    でオブジェクトを生成する。
    create_<関連先>は、has_one関連づけをすると使用できるメソッドで、今回の場合はcartsテーブルにuser_idが保存される。
    
    その後の処理は共通で、
    パラメーターで送られてきた商品IDを元にレコードを検索して変数に代入。
    cart.cart_products.create(product_id: product.id)でcart_idとporduct_idを中間テーブルに保存。
    商品詳細ページにリダイレクト。

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,4 +1,15 @@
 class CartProductsController < ApplicationController
   def create
+    if current_user.cart  # ログインユーザーが買い物カゴを持っている場合
+      cart = current_user.cart
+      product = Product.find_by(id: params[:product_id])  # パラメーターで送られてきた商品IDを元にレコードを代入
+      cart.cart_products.create(product_id: product.id)  # cart_idとproduct_idを保存
+      redirect_to product_path(product), notice: "カートに追加しました"
+    else  # ログインユーザーが買い物カゴを持っていない場合
+      cart = current_user.create_cart  # create_cartは、has_one関連づけをすると使用できるメソッドで、.create_<関連先>とすることでオブジェクトを生成できる。つまりこの場合はログインユーザーに紐づくカートが生成される（cartsテーブルにuser_idが保存される）。
+      product = Product.find_by(id: params[:product_id])
+      cart.cart_products.create(product_id: product.id)
+      redirect_to product_path(product), notice: "カートに追加しました"
+    end
   end
 end
