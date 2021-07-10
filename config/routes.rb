6e159f56c932429a40c66bd84c73a7db99c8f674
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-10 11:13:53 +0900

    カート内商品の削除のルーティング
    resourcesではなくresourceを使うとindex以外のURLが:idなしで生成される。
    カート内の商品はユーザーに紐づくため、「:id」による絞り込みは必要ないので、resourceを使用している。

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   }
   resources :products do
     resources :cart_products, only: [:create]
+    resource :cart_products, only: [:destroy]
   end
   resources :cart_products, only: [:index]
 end
