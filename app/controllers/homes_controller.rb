03f25cdfa1defd7cce9651cc85b5632a97171407
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-10 11:20:07 +0900

    購入完了ページへ遷移させるためのルーティングとアクション定義
    homeコントローラーへのcompleteアクションへのルーティングとアクションを定義。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def top
   end
+
+  def complete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     resource :cart_products, only: [:destroy]
   end
   resources :cart_products, only: [:index]
+  get '/complete', to: 'homes"complete'
 end
