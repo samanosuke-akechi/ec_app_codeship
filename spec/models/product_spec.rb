cce92d084247cec7d2adc01e5bfc9458a7df7944
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-13 13:11:09 +0900

    '商品名が入力されていない場合'の'商品登録が失敗すること'のテストコード
    変数productにオブジェクトを代入。
    product.valid?で変数productが正しいかを確認。
    expect(product.errors[:name]).toでnameカラムに対してエラーが出ていることを期待して、include("を入力してください")でエラーメッセージに「を入力してください」が含まれていることを想定。

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe Product, type: :model do
 
   context '商品名が入力されていない場合' do
     it '商品登録が失敗すること' do
+      product = Product.new(
+        name: "",
+        description: "テスト商品です",
+        price: 100
+      )
+
+      product.valid?
+      expect(product.errors[:name]).to include("を入力してください")
     end
   end
 end
