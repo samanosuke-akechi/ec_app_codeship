7075753db3230b09b079b320ae8e80dc48d75379
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-13 12:50:48 +0900

    テストコードが実行できるか確認

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'emailが入力されている場合' do
+    it 'ユーザー登録が成功すること' do
+    end
+  end
+
+  context 'emailが入力されていない場合' do
+    it 'ユーザー登録が失敗すること' do
+    end
+  end
+
+end
