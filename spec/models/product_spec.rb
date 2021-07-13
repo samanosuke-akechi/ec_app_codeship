9ad0ea291aa4be47b71220cfff3c0aa2132b6fe7
samanosuke-akechi <78411965+samanosuke-akechi@users.noreply.github.com>
2021-07-13 13:00:49 +0900

    '商品名が入力されている場合'の'商品登録が成功すること'のテストコード
    変数productにnameなどのデータが入ったオブジェクトを生成。
    expect(product).to be_validでproductの中のデータが有効かを期待するコードになっている。
    expect()の引数に期待する値を入れて、be_validは有効であることを期待するという意味。

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '商品名が入力されている場合' do
+    it '商品登録が成功すること' do
+      product = Product.new(
+        name: "商品A",
+        description: "テスト商品です",
+        price: 100
+      )
+
+      expect(product).to be_valid
+    end
+  end
+
+  context '商品名が入力されていない場合' do
+    it '商品登録が失敗すること' do
+    end
+  end
 end
